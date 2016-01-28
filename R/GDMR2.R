15b0f1c058c1f56ec78ea9ac20959059ad25d424
rakelup <raquelurenaperez@gmail.com>
2016-01-28 12:09:41 +0100

    Changing the description file

--- R/GDMR2.R
+++ R/GDMR2.R
@@ -2,19 +2,19 @@
 
 distanceCalculation=function(a,b,type)
 {
-  
-  if (type=='manhattan'){d=manhattan(a,b);} 
+
+  if (type=='manhattan'){d=manhattan(a,b);}
   else if (type=='euclidean') {d=euclidean(a,b);}
   else if (type=='cosine') {d=cosine(a,b);}
   else if(type=='dice') {d=dice(a,b);}
   else if(type=='jacard') {d=jacard(a,b);}
   return(d);
-  
+
 }
 
 manhattan=function(a,b)
 {
-  
+
   d=sum(abs(a-b));
   return(d)
 }
@@ -43,13 +43,13 @@ dice=function(a,b)
 jacard=function(a,b)
 {
   den=sqrt(sum(a*a))+sqrt(sum(b*b))-sqrt(sum(a*b));
-  d=sum(a*b)/den;  
+  d=sum(a*b)/den;
   return(d);
 }
 
 weight=function(r,type)
 {
-  
+
   if( type=='leasthalf')
   {
     a=0;
@@ -66,7 +66,7 @@ weight=function(r,type)
   if (r>=0 && r<a){q=0;}
   else if(r>=a && r<=b){q=(r-a)/(b-a);}
   else q=1;
-  
+
   return(q);
 }
 
@@ -75,12 +75,12 @@ agregation=function(preferences,M,N,quantifier)
   w=array(0,M);
   agregated=data.frame(matrix(0,N,N));
   a=matrix(0,1,M);
-  
+
   print('Estoy en agregacion GDMR2')
   for(i in 1:M){
     w[i]=weight(i/M,quantifier)-weight((i-1)/M,quantifier);
   }
-  
+
   agregated=apply(preferences,2,owa,w= w)
   return(agregated);
 }
@@ -103,7 +103,7 @@ exploitation=function(agregated,N,dominance,quantifier)
       ps[j]=max(c(agregated[j,i]-agregated[i,j],0));
     }
     GNDD[i]=owa(1-ps,w);
-    
+
   }
   #GDD
   #GNDD
@@ -115,7 +115,7 @@ exploitation=function(agregated,N,dominance,quantifier)
   tabla=rbind(GDD/max(GDD),GNDD/max(GNDD));
   rownames(tabla)=c('GDD','GNDD');
   colnames(tabla) <- 1:N;
-  
+
   return (tabla);
 }
 
@@ -129,85 +129,85 @@ owa=function(A,w){
 proximity=function(M,N,preferences,agregated)
 {
   #***********CONSENSUS MEASURES************************************
-  #Level 1 Preference level 
-  #Obtengo la Combinaci?n de m elemetos tomados de n en matrices de dimensi?n nxn 
+  #Level 1 Preference level
+  #Obtengo la Combinaci?n de m elemetos tomados de n en matrices de dimensi?n nxn
   #browser();
-  
+
   npref=N*N-N;
-  
-  
+
+
   prox_level1=data.frame(matrix(0,M,npref));
   dist_level1=data.frame(matrix(0,M,npref));
   prox_level2=data.frame(matrix(0,M,N));
   dist_level2=data.frame(matrix(0,M,N));
   aux=vector();
 
-  
+
   cont=1;
-  
+
   for (m1 in 1:M)
   {
-   
+
         for (i in 1:npref)
         {
-          
+
           a=preferences[m1,i];
           b=agregated[i];
           dist_level1[m1,i]=distanceCalculation(a,b,distance);
-          
-          
+
+
         }
 
-    
+
   }
   prox_level1=1-dist_level1;
   #print(prox_level1)
-  
+
   #level11=apply(preferences,1,distanceCalculation, b, distance)
-  
-  
+
+
   #print(level1);
   #browser();
-  
+
   #Level 2 Alternative Level
-  
-  
+
+
   cont=1;
-  
-  
+
+
   for (m in 1:M)
   {
-    
-    for (i in seq(1, ((N-1)*(N-1)+1), N-1)) 
-    {          
+
+    for (i in seq(1, ((N-1)*(N-1)+1), N-1))
+    {
       a=preferences[m,i:(i+N-2)];
       b=agregated[i:(i+N-2)];
       valor=distanceCalculation(a,b,distance);
       aux=c(aux,valor)
-      
+
     }
     dist_level2[cont,]=aux;
     cont=cont+1;
     aux=vector();
-    
+
   }
   prox_level2=1-dist_level2;
   #print(prox_level2)
-  
 
-  
-  
+
+
+
   #Proximity level3
-  
+
   proximity_level3_experts<<- apply(prox_level2, 1, mean);
   #barplot( proximity_level3_experts, main="Experts' Proximity level to the agregated solution",  xlab="Expert")
-  
+
   proximity_level3_alternatives<<- apply(prox_level2, 2, mean);
   #barplot( proximity_level3_alternatives, main="Alternatives' Proximity to the aggregated solution",  xlab="Alternative")
-  
-  
+
+
   #print(proximity_level3_experts)
-  
+
 }
 
 
@@ -217,17 +217,17 @@ proximity=function(M,N,preferences,agregated)
 consensus=function(M,N,preferences,distance)
 {
   #***********CONSENSUS MEASURES************************************
-  #Level 1 Preference level 
-  #Obtengo la Combinaci?n de m elemetos tomados de n en matrices de dimensi?n nxn 
+  #Level 1 Preference level
+  #Obtengo la Combinaci?n de m elemetos tomados de n en matrices de dimensi?n nxn
   #browser();
-  
+
   npref<<-N*N-N;
   numMa<<-factorial(M)/(factorial(2)*factorial(M-2));
   consensusExperts<<-data.frame(matrix(0,M,M))
   level1<<-data.frame(matrix(0,numMa,npref));
-  
+
   cont=1;
-  
+
   for (m1 in 1:M)
   {
     for (m2 in (m1+1):M)
@@ -235,46 +235,46 @@ consensus=function(M,N,preferences,distance)
       if (cont<=numMa){
         for (i in 1:npref)
         {
-          
+
           a=preferences[m1,i];
           b=preferences[m2,i];
           level1[cont,i]=distanceCalculation(a,b,distance);
-          
-          
+
+
         }
       }
-      
+
       cont=cont+1;
     }
   }
-  
+
 
   #print(level1);
   #browser();
-  
-  #Level 2 Alternative Level
-  
 
-  
+  #Level 2 Alternative Level
+
+
+
   distance_level2<<-data.frame(matrix(0,numMa,N));
   cont=1;
-  
+
   for (m1 in 1:(M-1))
-  { 
-    
+  {
+
     for (m2 in (m1+1):M)
     {
       aux=vector();
       i=1;
-      
+
       if (cont<=numMa){
-        for (i in seq(1, ((N-1)*(N-1)+1), N-1)) 
-        {          
+        for (i in seq(1, ((N-1)*(N-1)+1), N-1))
+        {
           a=preferences[m1,i:(i+N-2)];
           b=preferences[m2,i:(i+N-2)];
           valor=distanceCalculation(a,b,distance);
           aux=c(aux,valor)
-          
+
         }
         #print(m1)
         #print(m2)
@@ -286,37 +286,37 @@ consensus=function(M,N,preferences,distance)
       # print(cont)
     }
   }
-  
+
   print('Distance between experts for each alternative, Position (1,1) Means distance between expert 1 and 2 for the first alternative')
   print('Position (1,2) means distance between expert 1,2 for de second alternative')
-  #print(distance_level2) 
-  
+  #print(distance_level2)
+
   #Similarity level2
   similarity_level2<<-data.frame(matrix(0,numMa,N));
   similarity_level2<<-1-distance_level2;
-  
+
   # Level 3 on experts
-  
+
   similarity_level3_experts<<-apply(similarity_level2, 1, mean);
-  
+
   mean_consensus_perExpert<<-apply( consensusExperts, 1, sum)/(M-1);
-  
+
   #barplot( similarity_level3_experts, main="Experts'  Similarity level ",  xlab="Expert",col=heat.colors(12))
   similarity_level3_alternatives<<- apply(similarity_level2, 2, mean);
-  
+
   #barplot( similarity_level3_alternatives, main="Alternatives'  Similarity level ",  xlab="Expert", col=heat.colors(12))
-  
-  
-  
+
+
+
   consesus_global<<-mean(similarity_level3_alternatives)
-  
+
   return (consesus_global);#,level1,level2,level3);
 }
 
 
 completenes=function(N,preference)
 {
-  
+
   numberIncom=length(which(is.na(preference)));
   completennesLevel=((N*N-N)-numberIncom)/(N*N-N);
   return (completennesLevel);
@@ -325,7 +325,7 @@ completenes=function(N,preference)
 
 dataFrame2Matrix=function(df,N)
 {
-  
+
   contador=1;
   matriz=matrix(NA,N,N)
   for (i in 1:N)
@@ -344,11 +344,11 @@ dataFrame2Matrix=function(df,N)
     }
   }
   return (matriz);
-  
+
 }
 
 matrix2dataFrame=function(matrix,N)
 {
-  
-  
+
+
 }
